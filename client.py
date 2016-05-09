f4a8fa2c11f82c67605ba87d74fd87a1cb69f557
Miguel Sánchez de León Peque <peque@neosit.es>
2016-05-09 22:33:04 +0200

    Remove superficial comments

--- client.py
+++ client.py
@@ -60,7 +60,6 @@ conn = sqlite3.connect('local.db')
 
 c = conn.cursor()
 
-# Create table
 c.execute('''
     CREATE TABLE IF NOT EXISTS log (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -69,16 +68,12 @@ c.execute('''
         value DOUBLE
     )''')
 
-# Insert a row of data
 data = [(datetime.today(), key, value) for key, value in data.items()]
 c.executemany('INSERT INTO log VALUES (NULL,?,?,?)', data)
 
-# Save (commit) the changes
 conn.commit()
 
 for row in c.execute('SELECT * FROM log ORDER BY date'):
     print(row)
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
 conn.close()
